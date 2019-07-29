90d20a4fec1a98ff16acc2de6fc1e80c29afa388
mjalbert13 <mjalbert13@gmail.com>
2019-07-29 16:18:25 -0400

    set up  mongoose models

---
+++ models/Articles.js
@@ -0,0 +1,24 @@
+var mongoose =require('mongoose');
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    headline:{
+        type: String,
+        required: true
+    },
+    summary: {
+        tpye: String,
+        required: true
+    },
+    link: {
+        tpye: String,
+        required: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+var Article = mongoose.model('Article', ArticleSchema);
+module.exports = Article;
